dae5cc4de3fb5cd4bf44349dcad098efabbaf55e
Parker Manning <parkerlmanning@hotmail.com>
2022-03-26 19:27:23 -0500

    added error handling to useApi hook

--- __tests__/TabOneScreen-test.js
+++ __tests__/TabOneScreen-test.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import TabOneScreen from './../screens/TabOneScreen';
 import  renderer  from 'react-test-renderer';
+import TabTwoScreen from '../screens/TabTwoScreen';
+
+
+// docs used for jest as I'm very new to testing  
+// https://jestjs.io/docs/tutorial-react-native 
 
 test('renders correctly', () => {
   const tree = renderer.create(<TabOneScreen />).toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+test('renders correctly', () => {
+  const tree = renderer.create(<TabTwoScreen />).toJSON();
+  expect(tree).toMatchSnapshot();
+})
